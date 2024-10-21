d4d091bbdd498c8cb41dbc868cd9f9cc1ff63f36
Mechetel <dima.homa5@gmail.com>
2024-10-21 10:09:31 +0300

    delete install file

--- INSTALL/copy_npmrc.rb
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env ruby
-
-# XXX: copy, instead of link, because npm adds secret access_token to it
-require_relative './config'
-
-`cp -f #{File.join($dotfiles, 'npmrc')} #{File.join($home, '.npmrc')}`
